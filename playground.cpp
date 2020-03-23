033a5acba4b60062c4282a4f96028f827cf2dfef
ZachJHansen <zachansen62@gmail.com>
2020-03-23 06:52:40 -0500

    Shmemified sliding_queue st each PE has a QueueBuffer that is flushed to a symmetric sliding queue (the frontier)

--- playground.cpp
+++ playground.cpp
@@ -1,16 +1,18 @@
-#include "src/generator.h"
-#include "shmem.h"
-#include "src/zachs_sliding_queue.h"
+#include "/home/zach/projects/GAPBS/gapbs/src/generator.h"
+#include "sliding_queue.h"
 #include <utility>
 
 #define True 1
+#define False 0
 
 typedef EdgePair<int, int> Edge;
 typedef pvector<Edge> EdgeList;
 typedef SlidingQueue<Edge> EdgeQ;
+typedef QueueBuffer<Edge> QB;
 
 int main(void)
 {
+    static long FRONTIER_LOCK = 0;                                                      // Create a mutex lock in symmetric memory to control access to the frontier
     shmem_init();
 
     int npes = shmem_n_pes();
@@ -22,18 +24,17 @@ int main(void)
     size_t *num_edges = (size_t*) shmem_malloc(sizeof(size_t));
     *num_nodes = 1l << scale;                                                           // N = 2^scale nodes
     *num_edges = *num_nodes * degree;
-    EdgeList* EL = (EdgeList*) shmem_calloc(*num_edges, sizeof(Edge));                   // Create space for an edge list in symmetric memory across all PEs.
+    EdgeList* EL = (EdgeList*) shmem_calloc(*num_edges, sizeof(Edge));                  // Create space for an edge list in symmetric memory across all PEs.
 
     long pSync[_SHMEM_BCAST_SYNC_SIZE];                                                 // Initialize symmetric work array pSync
     size_t *el_size = (size_t*) shmem_malloc(sizeof(size_t));                           // Create a size_t slot in symmetric memory called el_size
 
-    Generator<int, int, int> genuine{scale, degree};                                // Generate an unweighted graph with 2^scale vertices, each with degree connections except it seems like degree is off
+    Generator<int, int, int> genuine{scale, degree};                                    // Generate an unweighted graph with 2^scale vertices, each with degree connections?
     *EL = genuine.GenerateEL(True);                                                     // Generate a uniform edge list in symmetric memory
 
-    // But first let's try building a sliding queue that can be modified by multiple PEs (frontier)
-    void* frontier_buffer = shmem_malloc(sizeof(EdgeQ));                                // Just to emphasize that malloc only creates space, placement new concept in c++
-    EdgeQ* frontier = new(frontier_buffer) EdgeQ{40};                                   // whereas new(frontier_buffer) EdgeQ is needed to call the edgeQ constructor
-    if (pe == 0){
+    void* frontier_alloc = shmem_malloc(sizeof(EdgeQ));                                // Just to emphasize that malloc only creates space, placement new concept in c++
+    EdgeQ* frontier = new(frontier_alloc) EdgeQ{40};                                   // whereas new(frontier_buffer) EdgeQ is needed to call the edgeQ constructor
+/*    if (pe == 0){
         for (auto it = (*EL).begin(); it < (*EL).end(); it++) {
             frontier->push_back((*it));
         }
@@ -41,11 +42,38 @@ int main(void)
         for (auto it = frontier->begin(); it < frontier->end(); it++) {
             printf("PE: %d | Src: %d | Dest: %d\n", pe, (*it).u, (*it).v);
         }
+    }*/
+    //EdgeQ& f = *frontier;
+    size_t sighs = 3;
+    QB* qb = new QB(*frontier, pe, npes, &FRONTIER_LOCK, sighs);
+    /*for (auto it = EL->begin(); it < EL->end(); it++) {
+        qb->push_back((*it));
+    }*/
+    for (int i = 0; i < sighs; i++){
+        qb->push_back(Edge(pe+10, pe+10));
     }
+    shmem_barrier_all();
+    qb->flush();
+    shmem_barrier_all();
+    frontier->slide_window();
+    shmem_barrier_all();
+    if (pe == 2){
+        for (auto it = frontier->begin(); it < frontier->end(); it++) {
+            printf("PE: %d | Src: %d | Dest: %d\n", pe, (*it).u, (*it).v);
+        }
+    }
+    shmem_barrier_all();
+    if (pe == 4){
+        for (auto it = frontier->begin(); it < frontier->end(); it++) {
+            printf("PE: %d | Src: %d | Dest: %d\n", pe, (*it).u, (*it).v);
+        }
+    }
+    //qb->push_back(*(EL->begin()));
+    
     // Next step is shmemifying the queue buffers (currently each thread manages a queuebuffer, we need each PE to manage a qb) 
     // Now we need a parent array in symmetric memory
     frontier->~EdgeQ();
-    shfree(frontier_buffer);
+    shfree(frontier_alloc);
     shfree(num_nodes);
     shfree(num_edges);
     shfree(EL);                                                         // Deallocate the edge_list on all PEs
---
+++ sliding_queue.h
@@ -0,0 +1,127 @@
+// Copyright (c) 2015, The Regents of the University of California (Regents)
+// See LICENSE.txt for license details
+
+#ifndef SLIDING_QUEUE_H_
+#define SLIDING_QUEUE_H_
+
+#include <algorithm>
+#include "shmem.h"
+#include "/home/zach/projects/GAPBS/gapbs/src/platform_atomics.h"
+
+
+/*
+GAP Benchmark Suite
+Class:  SlidingQueue
+Author: Scott Beamer
+
+Double-buffered queue so appends aren't seen until SlideWindow() called
+ - Use QueueBuffer when used in parallel to avoid false sharing by doing
+   bulk appends from thread-local storage
+
+    Reworked in such a way that an individual PE does the work of a thread
+    When the sliding queue in symmetric memory is updated by one PE, other PEs must wait to access
+*/
+
+
+template <typename T>
+class QueueBuffer;
+
+template <typename T>
+class SlidingQueue {
+  T *shared;
+  size_t shared_in;
+  size_t shared_out_start;
+  size_t shared_out_end;
+  friend class QueueBuffer<T>;
+
+ public:
+  explicit SlidingQueue(size_t shared_size) {
+    shared = (T *) shmem_calloc(shared_size, sizeof(T));
+    reset();
+  }
+
+  ~SlidingQueue() {
+    shfree(shared);
+  }
+
+  void push_back(T to_add) {
+    shared[shared_in++] = to_add;
+  }
+
+  bool empty() const {
+    return shared_out_start == shared_out_end;
+  }
+
+  void reset() {
+    shared_out_start = 0;
+    shared_out_end = 0;
+    shared_in = 0;
+  }
+
+  void slide_window() {
+    shared_out_start = shared_out_end;
+    shared_out_end = shared_in;
+  }
+
+  typedef T* iterator;
+
+  iterator begin() const {
+    return shared + shared_out_start;
+  }
+
+  iterator end() const {
+    return shared + shared_out_end;
+  }
+
+  size_t size() const {
+    return end() - begin();
+  }
+};
+
+
+template <typename T>
+class QueueBuffer {
+  size_t in;
+  T *local_queue;
+  SlidingQueue<T> &sq;
+  const size_t local_size;
+  int pe;
+  int npes; 
+  int locked;
+  long* FRONTIER_LOCK;
+
+ public:
+  explicit QueueBuffer(SlidingQueue<T> &master, int my_pe, int num_pes, long* FL, size_t given_size = 16384)
+      : sq(master), pe(my_pe), npes(num_pes), FRONTIER_LOCK(FL), local_size(given_size) {
+    in = 0;
+    local_queue = new T[local_size];
+  }
+
+  ~QueueBuffer() {
+    delete[] local_queue;
+  }
+
+  void push_back(T to_add) {
+    if (in == local_size)
+      flush();
+    local_queue[in++] = to_add;
+  }
+
+  void flush() {
+    shmem_set_lock(FRONTIER_LOCK);                                                      // Lock critical region (the frontier) to avoid simultaneous flushes
+    T *shared_queue = sq.shared;
+    size_t copy_start = shmem_ulong_atomic_fetch_add(&(sq.shared_in), in, pe);          // Get start of shared queue incoming region, update local copy of incoming region start position
+    size_t copy_end = copy_start + local_size;
+    std::copy(local_queue, local_queue+in, shared_queue+copy_start);                    // Update local copy of shared queue
+    for (int i = 0; i < npes; i++){
+        if (i != pe){
+            shmem_put64(shared_queue+copy_start, local_queue, local_size, i);           // Update shared queue on all PEs (An Edge<int, int> consumes 8 bytes of memory)
+            shmem_ulong_put(&(sq.shared_in), &copy_end, (long unsigned) 1, i);          // Move start of incoming region to end of copied elements
+        }
+    }
+    in = 0;
+    shmem_clear_lock(FRONTIER_LOCK);
+  }
+};
+
+#endif  // SLIDING_QUEUE_H_
