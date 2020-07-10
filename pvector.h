666a03c8d30f47ed120298c749514118a193cd7f
ZachJHansen <zachansen62@gmail.com>
2020-07-10 05:42:34 -0500

    Added round robin partition struct

--- pvector.h
+++ pvector.h
@@ -74,7 +74,77 @@ template <typename T_=int64_t> struct Partition {
 //    shmem_set_lock(PRINT_LOCK);
 };
 
+template <typename T_> struct RoundRobin {
+  int pe, npes;
+  T_ *local_width_, *max_width_;
+  
+  RoundRobin() {
+    pe = shmem_my_pe();
+    npes = shmem_n_pes();
+    local_width_ = (T_*) shmem_malloc(sizeof(T_));
+    *local_width_ = 0;
+    max_width_ = (T_*) shmem_malloc(sizeof(T_));
+    *max_width_ = -1;
+  }
 
+  // which PE should K be assigned to?
+  int owner(T_ k) {
+    if (k < 1) {
+      printf("Can't partition < 1 element! Breaking...\n");
+      shmem_global_exit(1);
+      exit(1);
+    }
+    if (k < npes) {
+      return(k-1);
+    } else {
+      if (k % npes == 0)
+        return(npes-1);
+      else
+        return((k % npes)-1);
+    }
+  }
+
+  void inc_local() {
+    (*local_width_)++;
+  }
+
+  T_ local_width() {
+    return *local_width_;
+  }
+
+  T_ max_width() {
+    if (max_width_ == -1) {
+      printf("Max width has not been finalized yet!\n");
+      return -1;
+    } else {
+      return *max_width_;      
+    }
+  }
+
+  // This should only be called once partitioning is over
+  // Must be called by all PEs
+  T_ finalize_max_width() {
+    long* temp_pSync = (long*) shmem_calloc(SHMEM_REDUCE_SYNC_SIZE, sizeof(SHMEM_SYNC_VALUE));
+    int* temp_pWrk = (int*) shmem_calloc(SHMEM_REDUCE_MIN_WRKDATA_SIZE, sizeof(SHMEM_SYNC_VALUE)); 
+    for (int i = 0; i < SHMEM_REDUCE_SYNC_SIZE; i++)
+      temp_pSync[i] = SHMEM_SYNC_VALUE;
+    for (int i = 0; i < SHMEM_REDUCE_MIN_WRKDATA_SIZE; i++)
+      temp_pWrk[i] = SHMEM_SYNC_VALUE;
+    shmem_barrier_all();
+    if (sizeof(T_) == sizeof(int)) {
+      shmem_int_max_to_all(max_width_, local_width_, 1, 0, 0, npes, temp_pWrk, temp_pSync);
+    } else {
+      printf("Haven't finished implementing generically typed RoundRobin\n");
+      shmem_global_exit(0);
+      exit(0);
+    } 
+    shmem_barrier_all();
+    shmem_free(temp_pSync);
+    shmem_free(temp_pWrk);
+    return *max_width_;
+  }
+};
+ 
 template <typename T_>
 class pvector {
  public:
@@ -143,6 +213,7 @@ class pvector {
   void reserve(size_t num_elements) {
     if (num_elements > capacity()) {
       if (symmetric_) { 
+        shmem_barrier_all();
         T_ *new_range = (T_ *) shmem_calloc(num_elements, sizeof(T_));
         #pragma omp parallel for
         for (size_t i=0; i < size(); i++)
