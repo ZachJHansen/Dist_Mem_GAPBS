901ca86657b97affccf83c721eb1adcbe66368c5
ZachJHansen <zachansen62@gmail.com>
2021-02-24 16:37:38 -0600

    time to download trash-cli

--- bc.cc
+++ bc.cc
@@ -91,7 +91,15 @@ void PBFS(const Graph &g, NodeID source, pvector<CountT> &path_counts,
 // path counts, depths are partitioned
 void shmem_BFS(const Graph &g, NodeID source, pvector<CountT> &path_counts,
     Bitmap &succ, vector<SlidingQueue<NodeID>::iterator> &depth_index,
-    SlidingQueue<NodeID> &queue, Partition<NodeID> vp, long* PATH_LOCK, long long* pWrk, long* pSync) {
+    SlidingQueue<NodeID> &queue, Partition<NodeID> vp, long* PATH_LOCK) {
+
+  static long BFSpSync[SHMEM_REDUCE_SYNC_SIZE];
+  static long long BFSpWrk[SHMEM_REDUCE_MIN_WRKDATA_SIZE];
+  for (int i = 0; i < SHMEM_REDUCE_SYNC_SIZE; i++)
+    BFSpSync[i] = SHMEM_SYNC_VALUE;
+  for (int i = 0; i < SHMEM_REDUCE_MIN_WRKDATA_SIZE; i++)
+    BFSpWrk[i] = SHMEM_SYNC_VALUE;
+
   pvector<long> depths(vp.max_width, -1, true);
   if (source >= vp.start && source < vp.end) {
     depths[vp.local_pos(source)] = 0;
@@ -109,53 +117,48 @@ void shmem_BFS(const Graph &g, NodeID source, pvector<CountT> &path_counts,
     auto iter_init = queue.begin();
     for (NodeID q_iter = qp.start; q_iter < qp.end; q_iter++) {
       NodeID u = *(iter_init+q_iter);
-      printf("U = %d\n", u);
       for (NodeID &v : g.out_neigh(u)) {
         NodeID lp_v = vp.local_pos(v);
         if ((shmem_long_g(depths.begin()+lp_v, vp.recv(v)) == -1) &&
-            (shmem_long_atomic_compare_swap(depths.begin()+lp_v, static_cast<long>(-1), depth, vp.recv(v)) == depth)) {
+            (shmem_long_atomic_compare_swap(depths.begin()+lp_v, static_cast<long>(-1), depth, vp.recv(v)) == -1)) {
         //if ((depths[v] == -1) &&
         //    (compare_and_swap(depths[v], static_cast<NodeID>(-1), depth))) {          // is the depths[v] == -1 comparison just for short circuit evaluation?
-          printf("PE %d pushing back %lu\n", vp.pe, v);
           lqueue.push_back(v);
         }
         if (shmem_long_g(depths.begin()+lp_v, vp.recv(v)) == depth) {
-          printf("PE %d setting bit %lu\n", vp.pe, v);
           succ.set_bit_partitioned(g, u, v, vp);
           // this command is supposed to be atomic, but requires 2 shmem instructions at best:
           // shmem_atomic_fetch to get path_counts[u], and shmem_atomic_add to add that value to path_counts[v]
           // so is a lock the only solution? maybe two locks, like lock 5 and lock 8 becomes 58
           shmem_set_lock(PATH_LOCK);            // should be test lock since we dont want everyone to exec this sequentially
-          printf("lp_u = %d, own_u = %d\n", vp.local_pos(u), vp.recv(u));
-          printf("pc[u] = %d\n", shmem_double_g(path_counts.begin()+vp.local_pos(u), vp.recv(u)));
           CountT pc_u = shmem_double_g(path_counts.begin()+vp.local_pos(u), vp.recv(u));
           CountT pc_v = shmem_double_g(path_counts.begin()+lp_v, vp.recv(v));
-          printf("PE %d combining path counts total = %d u = %d, v = %d\n", vp.pe, pc_u, pc_v, );
+          printf("PE %d combining path counts total = %f u = %f, v = %f\n", vp.pe, pc_u+pc_v, pc_u, pc_v);
           shmem_double_p(path_counts.begin()+lp_v, pc_u+pc_v, vp.recv(v));
           shmem_clear_lock(PATH_LOCK);
           //path_counts[v] += path_counts[u];
         }
       }
     }
-    printf("PE %d is about to flush\n", vp.pe);
     lqueue.flush();
     shmem_barrier_all();
     depth_index.push_back(queue.begin());
     queue.slide_window();                               // synch point
   }
+  //lqueue.flush();
   shmem_barrier_all();
-  succ.merge(pWrk, pSync);
+  succ.merge(BFSpWrk, BFSpSync);
   depth_index.push_back(queue.begin());
 }
 
 
 pvector<ScoreT> Brandes(const Graph &g, SourcePicker<Graph> &sp,
-                        NodeID num_iters, long* PATHLOCK, long* QLOCK, long long* pWrk, long* pSync) {
+                        NodeID num_iters, long* PATHLOCK, long* QLOCK, float* pWrk, long* pSync) {
   Timer t;
   t.Start();
   Partition<NodeID> vp(g.num_nodes());
   pvector<ScoreT> scores(vp.max_width, 0, true);                // symmetric partitioned pvector
-  pvector<CountT> path_counts(vp.max_width, 0, true);                   // symmetric partitioned pvector
+  pvector<CountT> path_counts(vp.max_width, true);                   // symmetric partitioned pvector
   Bitmap succ(g.num_edges_directed(), true);                          // symmetric non-partitioned bitmap
   succ.init_bitmap_offsets(g, vp);
   vector<SlidingQueue<NodeID>::iterator> depth_index;
@@ -170,10 +173,10 @@ pvector<ScoreT> Brandes(const Graph &g, SourcePicker<Graph> &sp,
     depth_index.resize(0);
     queue.reset();
     succ.reset();
-    shmem_BFS(g, source, path_counts, succ, depth_index, queue, vp, PATHLOCK, pWrk, pSync);
+    shmem_BFS(g, source, path_counts, succ, depth_index, queue, vp, PATHLOCK);
     printf("Pe %d Path counts: ", vp.pe);
     for (NodeID n = vp.start; n < vp.end; n++)
-      printf("%d ", path_counts[vp.local_pos(n)]);
+      printf("%f ", path_counts[vp.local_pos(n)]);
     printf("\n");
     printf("PE %d successors: ", vp.pe);
     for (NodeID n = 0; n < g.num_edges_directed(); n++)
@@ -205,19 +208,18 @@ pvector<ScoreT> Brandes(const Graph &g, SourcePicker<Graph> &sp,
     PrintStep("p", t.Seconds());
   }
 
-  shmem_barrier_all();
-  shmem_global_exit(0);
-  exit(0);
-  for (auto t : scores)
-    printf("PE %d | %f\n", vp.pe, t);
+  //for (auto t : scores)
+    //printf("PE %d | %f\n", vp.pe, t);
   // normalize scores
-  /*ScoreT biggest_score = 0;
-  #pragma omp parallel for reduction(max : biggest_score)
-  for (NodeID n=0; n < g.num_nodes(); n++)
-    biggest_score = max(biggest_score, scores[n]);
-  #pragma omp parallel for
-  for (NodeID n=0; n < g.num_nodes(); n++)
-    scores[n] = scores[n] / biggest_score;*/
+  ScoreT* biggest_score = (float *) shmem_malloc(sizeof(float));
+  *biggest_score = 0;
+  //#pragma omp parallel for reduction(max : biggest_score)
+  for (NodeID n=vp.start; n < vp.end; n++)
+    *biggest_score = max(*biggest_score, scores[vp.local_pos(n)]);
+  shmem_float_max_to_all(biggest_score, biggest_score, 1, 0, 0, vp.npes, pWrk, pSync);
+  //#pragma omp parallel for
+  for (NodeID n=vp.start; n < vp.end; n++)
+    scores[vp.local_pos(n)] = scores[vp.local_pos(n)] / *biggest_score;
   return scores;
 }
 
@@ -240,6 +242,18 @@ void PrintTopScores(const Graph &g, const pvector<ScoreT> &scores) {
 // - regenerates successors from depths
 bool BCVerifier(const Graph &g, SourcePicker<Graph> &sp, NodeID num_iters,
                 const pvector<ScoreT> &scores_to_test) {
+  Partition<NodeID> vp(g.num_nodes());
+  int* PRINTER = (int *) shmem_malloc(sizeof(int));
+  *PRINTER = 0;
+  shmem_barrier_all();
+  shmem_int_wait_until(PRINTER, SHMEM_CMP_EQ, vp.pe);       // wait until previous PE puts your pe # in PRINTER
+  ofstream shmem_out;
+  shmem_out.open("/home/zach/projects/Dist_Mem_GAPBS/Dist_Mem_GAPBS/bc_output.txt", ios::app);
+  for (NodeID n = vp.start; n < vp.end; n++)
+    shmem_out << scores_to_test[vp.local_pos(n)] << endl;
+  shmem_out.close();
+  if (!(vp.pe == vp.npes-1))
+    shmem_int_p(PRINTER, vp.pe+1, vp.pe+1);     
   return true;
 }
 
@@ -260,15 +274,13 @@ int main(int argc, char* argv[]) {
 
   static long pSync[SHMEM_REDUCE_SYNC_SIZE];
   static long lng_pWrk[SHMEM_REDUCE_MIN_WRKDATA_SIZE];      
-  static long long ll_pWrk[SHMEM_REDUCE_MIN_WRKDATA_SIZE];
-  static double dbl_pWrk[SHMEM_REDUCE_MIN_WRKDATA_SIZE];                 
+  static float flt_pWrk[SHMEM_REDUCE_MIN_WRKDATA_SIZE];                 
 
   for (int i = 0; i < SHMEM_REDUCE_SYNC_SIZE; i++)
     pSync[i] = SHMEM_SYNC_VALUE;
   for (int i = 0; i < SHMEM_REDUCE_MIN_WRKDATA_SIZE; i++) {
     lng_pWrk[i] = SHMEM_SYNC_VALUE;
-    ll_pWrk[i] = SHMEM_SYNC_VALUE;
-    dbl_pWrk[i] = SHMEM_SYNC_VALUE;
+    flt_pWrk[i] = SHMEM_SYNC_VALUE;
   }
 
   {
@@ -276,7 +288,7 @@ int main(int argc, char* argv[]) {
     Graph g = b.MakeGraph(pSync, lng_pWrk);
     SourcePicker<Graph> sp(g, cli.start_vertex());
     auto BCBound =
-      [&sp, &cli] (const Graph &g) { return Brandes(g, sp, cli.num_iters(), &PATHLOCK, &QLOCK, ll_pWrk, pSync); };
+      [&sp, &cli] (const Graph &g) { return Brandes(g, sp, cli.num_iters(), &PATHLOCK, &QLOCK, flt_pWrk, pSync); };
     SourcePicker<Graph> vsp(g, cli.start_vertex());
     auto VerifierBound = [&vsp, &cli] (const Graph &g,
                                      const pvector<ScoreT> &scores) {
